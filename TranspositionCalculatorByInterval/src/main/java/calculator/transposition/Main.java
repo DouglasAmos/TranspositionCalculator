f0d283caa1a9275168ae13a536509a378db952cf
Douglas Amos <douglas.amos1@gmail.com>
2023-11-19 15:06:13 -0500

    Putting in fixes and improvements to interval calculator

--- TranspositionCalculatorByInterval/src/main/java/calculator/transposition/Main.java
+++ TranspositionCalculatorByInterval/src/main/java/calculator/transposition/Main.java
@@ -3,6 +3,7 @@ package calculator.transposition;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -15,7 +16,7 @@ public class Main {
         Scanner inputScanner = new Scanner(System.in);
         final String[] notesInScale = new String[]{"C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B"};
 
-        final String[] intervalString = new String[]{"placeholder", "m2", "M2", "m3", "M3", "P4", "TT", "P5", "m6", "M6", "m7", "M7"};
+        final String[] intervalString = new String[]{"", "m2", "M2", "m3", "M3", "P4", "TT", "P5", "m6", "M6", "m7", "M7"};
 
         System.out.println("Would you like transpose a note? (yes/no)");
 
@@ -32,6 +33,11 @@ public class Main {
             System.out.println("Please enter your starting note:");
             String userInputStartNote = inputScanner.nextLine();
 
+            while (!userInputStartNote.matches(Arrays.toString(notesInScale))) {
+                System.out.println("Please enter a valid starting note");
+                userInputStartNote = inputScanner.nextLine();
+            }
+
 
             System.out.println("Are you transposing upward (U), or downward (D)?");
             String userInputDirection = inputScanner.nextLine().toUpperCase();
@@ -44,6 +50,14 @@ public class Main {
 
             System.out.println("Please enter your desired interval to transpose using lowercase for minor and uppercase for major: ");
             String userInputInterval = inputScanner.nextLine();
+
+            /*
+            while (!userInputInterval.matches(Arrays.toString(intervalString))) {
+                System.out.println("Please enter a valid interval");
+                userInputInterval = inputScanner.nextLine();
+            }
+
+             */
 
 
             int transpoNum = 0;
