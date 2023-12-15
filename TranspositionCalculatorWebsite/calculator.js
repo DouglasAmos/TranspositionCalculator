3badcba93d851b4f89348f0033f82e9b12a8ef42
Douglas Amos <douglas.amos1@gmail.com>
2023-12-14 20:06:59 -0500

    making website and putting calculator into JavaScript

---
+++ TranspositionCalculatorWebsite/calculator.js
@@ -0,0 +1,16 @@
+const notesInScale = ["C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G",
+"G#/Ab", "A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb",
+"B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B"];
+
+const intervals = ["U", "m2", "M2", "m3", "M3", "P4", "TT", "P5", "m6", "M6", "m7", "M7"]
+
+
+
+
+function transpose(startingNote, direction, interval){
+
+
+
+
+
+}
