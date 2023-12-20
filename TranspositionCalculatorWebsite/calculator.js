241bf7df86bef0103979faf7d7c86655a9548dc6
Douglas Amos <douglas.amos1@gmail.com>
2023-12-20 14:33:48 -0500

    working calculator on website

--- TranspositionCalculatorWebsite/calculator.js
+++ TranspositionCalculatorWebsite/calculator.js
@@ -5,10 +5,36 @@ const notesInScale = ["C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G",
 const intervals = ["U", "m2", "M2", "m3", "M3", "P4", "TT", "P5", "m6", "M6", "m7", "M7"]
 
 
+document.addEventListener("DOMContentLoaded", (event) =>{
+
+    const runButton = document.getElementById("transpoButton");
+
+    runButton.addEventListener("click", (event) => {
+
+       const dropDown1 = document.getElementById("starting-note");
+       const startingNote = dropDown1.value;
+       const dropDown2 = document.getElementById("direction");
+       const direction = dropDown2.value;
+       const dropDown3 = document.getElementById("interval")
+       const interval = dropDown3.value;
+
+        answer = transpose(startingNote, direction, interval);
+
+        const h3 = document.getElementById("answer");
+        h3.innerText = answer;
+
+    })
+
+
+
+})
+
+
 
 
 function transpose(startingNote, direction, interval){
 
+    let goalNote;
 
     let intervalNum = 0;
     for (let i = 0; i < intervals.length; i++){
@@ -17,19 +43,19 @@ function transpose(startingNote, direction, interval){
         }
     }
 
-    if(direction === "D"){
+    if(direction === "Downward"){
         intervalNum *= -1;
     }
 
     for(let i = 12; i < 24; i++){
         if(notesInScale[i] === startingNote){
-            let goalNote = notesInScale[i + intervalNum];
+            goalNote = notesInScale[i + intervalNum];
 
-            return goalNote;
+            
         }
     }
 
-    
+    return goalNote;
 
 
 
