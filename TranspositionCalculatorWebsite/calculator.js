01a5dbd85942090dc6a7d4865969658e0608bd8d
Douglas Amos <douglas.amos1@gmail.com>
2023-12-20 14:46:05 -0500

    a few bug fixes

--- TranspositionCalculatorWebsite/calculator.js
+++ TranspositionCalculatorWebsite/calculator.js
@@ -2,7 +2,7 @@ const notesInScale = ["C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G",
 "G#/Ab", "A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb",
 "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B"];
 
-const intervals = ["U", "m2", "M2", "m3", "M3", "P4", "TT", "P5", "m6", "M6", "m7", "M7"]
+const intervals = ["U", "m2", "M2", "m3", "M3", "P4", "Tritone", "P5", "m6", "M6", "m7", "M7"]
 
 
 document.addEventListener("DOMContentLoaded", (event) =>{
