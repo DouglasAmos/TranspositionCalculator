c563c1231eeb44f799e72c692242f01ce883aaa8
Douglas Amos <douglas.amos1@gmail.com>
2023-12-20 11:03:08 -0500

    Built working calculator in JS

--- TranspositionCalculatorWebsite/calculator.js
+++ TranspositionCalculatorWebsite/calculator.js
@@ -10,6 +10,26 @@ const intervals = ["U", "m2", "M2", "m3", "M3", "P4", "TT", "P5", "m6", "M6", "m
 function transpose(startingNote, direction, interval){
 
 
+    let intervalNum = 0;
+    for (let i = 0; i < intervals.length; i++){
+        if(interval === intervals[i]){
+            intervalNum = i;
+        }
+    }
+
+    if(direction === "D"){
+        intervalNum *= -1;
+    }
+
+    for(let i = 12; i < 24; i++){
+        if(notesInScale[i] === startingNote){
+            let goalNote = notesInScale[i + intervalNum];
+
+            return goalNote;
+        }
+    }
+
+    
 
 
 
